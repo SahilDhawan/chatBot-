5f099f9a803eab4e3c46e2d4d6b991a25870b635
Sahil Dhawan <sahildhawan2012@icloud.com>
2016-08-07 12:41:22 +0530

    quote

--- fb_chatBot/views.py
+++ fb_chatBot/views.py
@@ -42,7 +42,7 @@ quotes_arr = [["Life isn’t about getting and having, it’s about giving and b
 
 
 def post_facebook_message(fbid, recevied_message):
-    reply_text = recevied_message + '. Anyways let\'s play Pokemon Go ;) .' 
+    reply_text = recevied_message + '. Anyways let\'s play Pokemon Go ;) .' + quotes_arr[0]
 
     try:
         user_details_url = "https://graph.facebook.com/v2.6/%s"%fbid 
